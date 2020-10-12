b42a669056476f43c0d2a546e1f3486f76d22454
Ismael Peña <sistemas@trr.com.mx>
2020-10-12 16:45:03 -0500

    12/10/20

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { TranslateModule } from '@ngx-translate/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { SectionHomeComponent } from './section-home/section-home.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    SectionHomeComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    TranslateModule.forRoot()
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
